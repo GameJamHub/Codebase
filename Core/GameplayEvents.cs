eb2980d04fc9b652016852b1c470f5866a99e000
Prajwal <prajwalr.ramappa@gmail.com>
2024-04-10 00:25:01 +0530

    Input, Events and singleton added

---
+++ Core/GameplayEvents.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+namespace Codebase.Core
+{
+    public partial class GameplayEvents
+    {
+        public static event Action<Vector2, bool> OnMovement;
+
+        public static void SendOnMovement(Vector2 axisValue, bool canMove)
+        {
+            OnMovement?.Invoke(axisValue, canMove);
+        }
+    }
+}
+
---
+++ Core/InputManager.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using Codebase.Core;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : Singleton<InputManager>
+{
+    private PlayerIA m_input = null;
+    
+    public override void Awake()
+    {
+        base.Awake();
+        m_input = new PlayerIA();
+    }
+    
+    private void OnEnable()
+    {
+        m_input.Enable();
+        m_input.Player.Movement.performed += OnMovementPerformed;
+        m_input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        m_input.Disable();
+        m_input.Player.Movement.performed -= OnMovementPerformed;
+        m_input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    public void EnableInput()
+    {
+        m_input.Enable();
+        m_input.Player.Movement.performed += OnMovementPerformed;
+        m_input.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    public void DisableInputs()
+    {
+        m_input.Disable();
+        m_input.Player.Movement.performed -= OnMovementPerformed;
+        m_input.Player.Movement.canceled -= OnMovementCancelled;
+    }
+    
+    private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        if (m_input == null)
+        {
+            return;
+        }
+
+        Vector2 axisValue = value.ReadValue<Vector2>();
+        GameplayEvents.SendOnMovement(axisValue, true);
+    }
+
+    private void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        if (m_input == null)
+        {
+            return;
+        }
+
+        Vector2 axisValue = value.ReadValue<Vector2>();
+        GameplayEvents.SendOnMovement(axisValue, true);
+    }
+}
